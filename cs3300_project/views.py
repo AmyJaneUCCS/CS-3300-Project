7b6363b41c03cbb8024d15599f6c0ca48dd56432
AmyJane <amyjane.reilly@gmail.com>
2023-11-05 13:42:48 -0700

    areilly: Adding a user model and creating different url paths

--- cs3300_project/views.py
+++ cs3300_project/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request): 
     return render(request, 'cs3300_project/index.html') 
+
+def account(request, user_id):
+    return render(request, 'cs3300_project/account.html')
+
+def yourAccount(request):
+    return account(request, 1)
+
+def yourClips(request):
+    return render(request, 'cs3300_project/clips.html')
+
+def yourSaved(request):
+    return render(request, 'cs3300_project/saved.html')
